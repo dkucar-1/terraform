324729f3199edbc7b07afa9fcf19c03895f3a743
Dorothy Kucar <dorothykucar@macbook-pro-80.lan>
2026-06-15 17:09:31 -0600

    using tfvars

--- 08/compute.tf
+++ 08/compute.tf
@@ -13,12 +13,12 @@ data "aws_ami" "ubuntu" {
 
 resource "aws_instance" "compute" {
   ami           = data.aws_ami.ubuntu.id
-  instance_type = var.ec2_instance_size
+  instance_type = var.ec2_instance_type
 
   root_block_device {
     delete_on_termination = true
-    volume_size           = var.ec2_volume_size
-    volume_type           = var.ec2_volume_type
+    volume_size           = var.ec2_volume_config.size
+    volume_type           = var.ec2_volume_config.type
   }
 }
 
--- 08/variables.tf
+++ 08/variables.tf
@@ -1,21 +1,23 @@
 
-variable "ec2_instance_size" {
+variable "ec2_instance_type" {
   description = "Only t2.micro and t3.micro instance types are allowed."
   type        = string
   default     = "t2.micro"
 
   validation {
-    condition     = contains(["t2.micro", "t3.micro"], var.ec2_instance_size)
+    condition     = contains(["t2.micro", "t3.micro"], var.ec2_instance_type)
     error_message = "Invalid instance type. Only t2.micro and t3.micro are allowed."
   }
 }
 
-variable "ec2_volume_size" {
-  type        = number
-  description = "The size of the EBS volume in GB."
-}
-
-variable "ec2_volume_type" {
-  type        = string
-  description = "The type of EBS volume. Valid values are 'gp2', 'gp3'"
+variable "ec2_volume_config" {
+  type = object({
+    size = number
+    type = string
+  })
+  description = "The configuration for the EBS volume."
+  default = {
+    size = 8
+    type = "gp3"
+  }
 }
